841b3b37db33d8b2ca019bcfb8c7114093e4d690
Eduardo Finotti <eduardo.finotti@bravi.com.br>
2017-12-26 15:38:35 -0200

    ajustando projeto

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -1,5 +1,5 @@
 class HomePage < SitePrism::Page
-    set_url "/"
+    set_url "/index.php?module=Home&action=index"
   
     element :create, '#quickcreatetop > a'
     element :oportunidade, '#quickcreatetop > ul > li:nth-child(3) > a'
--- features/step_definitions/oportunidade/cadastrar_oportunidade.rb
+++ features/step_definitions/oportunidade/cadastrar_oportunidade.rb
@@ -1,37 +1,23 @@
-home = HomePage.new
-
-Dado("que eu estou na tela de login") do
-    home.load
-end
-
-Quando("preencho meus dados de login") do
-    login_page.login.set 'eduardo.finotti@softplan.com.br'
-    login_page.senha.set 'amarelo32'
-end
-
-Quando("clico no botão entrar") do
-    login_page.botaoEntrar.click
-end
-
-Então("Eu logo na aplicação com sucesso") do
-    expect(page).to have_link("Minha conta")
-end
-
-#################################################
+home_page = HomePage.new
+oport_page = OportunidadePage.new
 
 Dado("que estou na tela de cadastro de oportunidades") do
-    home.create.click
-    home.oportunidade.click
+    home_page.create.click
+    home_page.oportunidade.click
 end
 
 Quando("preencho os dados obrigatorios") do
-pending # Write code here that turns the phrase above into concrete actions
+    oport_page.name.set 'name'
+    oport_page.account_name.set 'RR. Talker Co'
+    sleep 3
+    oport_page.date_closed.set '01/10/2020'
+    oport_page.amount.set '123'
 end
 
 Quando("clico em salvar") do
-pending # Write code here that turns the phrase above into concrete actions
+    oport_page.save.click
 end
 
 Então("a oportunidade é salva corretamente") do
-pending # Write code here that turns the phrase above into concrete actions
+    expect(page).to have_content('ACTIONS')
 end
